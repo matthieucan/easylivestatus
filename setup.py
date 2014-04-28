f966af15e57c06e3f7b63366fcca7861a4bffe44
Matthieu Caneill <matthieu.caneill@savoirfairelinux.com>
2014-04-28 14:27:51 -0400

    add installation setip

---
+++ setup.py
@@ -0,0 +1,30 @@
+# This file is part of easylivestatus.
+# easylivestatus is a library to easily create LiveStatus queries.
+#
+# Copyright (C) 2014, Matthieu Caneill <matthieu.caneill@savoirfairelinux.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from setuptools import setup
+
+setup(name='easylivestatus',
+      version='0.1alpha',
+      description='A library to easily create LiveStatus queries.',
+      url='http://github.com/matthieucan/easylivestatus',
+      author='Matthieu Caneill',
+      author_email='matthieu.caneill@savoirfairelinux.com',
+      license='GPL3',
+      packages=['easylivestatus'],
+      zip_safe=False)
